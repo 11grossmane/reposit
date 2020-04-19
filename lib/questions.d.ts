ddf6ca8ba0e6febbd9b7d4a8a6339412fc5735bb
Eric Grossman <eric.josh.grossman@gmail.com>
2020-04-19 16:41:58 -0400

    removed lib

--- lib/questions.d.ts
+++
@@ -1,3 +0,0 @@
-import { Answers, Provider } from './types';
-export declare const questionsWithLogin: (provider: Provider) => Promise<Answers>;
-export declare const questionsIfCachedLogin: (provider: Provider) => Promise<Answers>;
