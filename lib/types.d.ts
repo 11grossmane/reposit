2daf8c93b2b3323ee55b142927d1f664c29b2303
Eric Grossman <eric.josh.grossman@gmail.com>
2020-04-20 19:35:01 -0400

    updated gitignore

---
+++ lib/types.d.ts
@@ -0,0 +1,38 @@
+export interface Answers {
+    username?: string;
+    password?: string;
+    repoName: string;
+}
+export interface Credentials {
+    Github?: GithubCredentials;
+    Bitbucket?: BitbucketCredentials;
+}
+export interface GithubCredentials {
+    username: string;
+    password: string;
+}
+export interface BitbucketCredentials {
+    username: string;
+    password: string;
+}
+export declare enum Provider {
+    GITHUB = "Github",
+    BITBUCKET = "Bitbucket"
+}
+export declare const GITHUB_REPO_URL = "https://api.github.com/user/repos";
+export declare const BITBUCKET_URL: (username: string, repoName: string) => string;
+export interface PostResponse {
+    repoName: string;
+    links: string[];
+    statusCode: number;
+}
+export interface PostResponse {
+    repoName: string;
+    links: string[];
+    statusCode: number;
+}
+export interface InternalArgs {
+    reset: boolean;
+    provider: Provider;
+}
+export declare const DATAPATH: string;
---
+++ lib/util.d.ts
@@ -0,0 +1,7 @@
+import { Credentials, Provider, BitbucketCredentials, PostResponse, GithubCredentials } from './types';
+export declare const clearCache: () => void;
+export declare const checkCache: () => Credentials | null;
+export declare const writeToCache: (newCredentials: Credentials) => void;
+export declare const hasCredentials: (credentials: Credentials | null, provider: Provider, reset: boolean) => boolean;
+export declare const bitbucketCreate: (credentials: BitbucketCredentials, repoName: string) => Promise<PostResponse>;
+export declare const githubCreate: (credentials: GithubCredentials, repoName: string) => Promise<PostResponse>;
